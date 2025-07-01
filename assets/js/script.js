ad89bd0b5e41172d73307b440fdb32a7f9774bc8
Kabir Hussain <145907171+Kabirh7700@users.noreply.github.com>
2025-07-01 18:15:09 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["MIS Executive", "Data analyst"],
+    strings: ["Data Analyst", "Data Analyst"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
